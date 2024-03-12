42e1081b94890c3f11614c92c878bd1839766c6b
bananaFish001 <raj123.rs308@gmail.com>
2024-03-12 23:02:17 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const Employee = require('./employee.js');
+const employee1 = new Employee('Gaurav', 'Freelancer');
+const employee2 = new Employee('vaibhav', 'Graphics Designer');
+
+employee1.info();
+employee2.info();
